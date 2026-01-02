94ddb0dc447dfb07f537fbe78e5d08af6e0c1e22
ohr486 <ohr486@gmail.com>
2026-01-02 19:15:24 +0900

    reset repo

--- src/preload.ts
+++
@@ -1,8 +0,0 @@
-import { contextBridge } from 'electron';
-
-contextBridge.exposeInMainWorld('api', {
-  getAppVersion: () => '1.0.0',
-  getElectronVersion: () => process.versions.electron,
-  getChromeVersion: () => process.versions.chrome,
-  getNodeVersion: () => process.versions.node
-});
